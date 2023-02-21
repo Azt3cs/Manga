d0d97bb7cfd0b257160c120bd386ba669e373883
Sh1fue <110296436+Sh1fue@users.noreply.github.com>
2023-02-21 22:30:14 +0300

    Add files via upload

---
+++ code/popa.js
@@ -0,0 +1,20 @@
+let i = 1
+let j =1
+const btnNext = document.querySelector('.next')
+btnNext.addEventListener('click', next)
+function next(){
+    const elem = document.querySelector('.img')
+    elem.classList.add('img'+i) 
+    i++
+    
+}
+
+const btnPrev = document.querySelector('.prev')
+btnPrev.addEventListener('click', prev)
+function prev(){
+    const elem = document.querySelector('.img')
+    elem.classList.remove('img'+i)
+    i--
+    
+   
+}
