fd104c9a6909d01cdd12a1829d1fabdbb732ef03
Alexandr Krainukov <krainukovaleksandr1@gmail.com>
2023-02-21 11:37:32 +0300

    pls commit changes

--- mangaapi/models.py
+++ mangaapi/models.py
@@ -2,30 +2,39 @@ import imghdr
 from django.db import models
 
 
-
 class Manga(models.Model):
- 
-    title = models.CharField(max_length=30,blank=False)
-    year = models.CharField(max_length=4,blank=False)
-    description = models.TextField()
-    author = models.CharField(max_length=20,blank=False)
-    genre =models.CharField(max_length=10,blank=False)
-
+    title = models.CharField(max_length=30, blank=False)
+    year = models.CharField(max_length=4, blank=False)
+    description = models.TextField(blank=False)
+    author = models.CharField(max_length=20, blank=False)
+    genre = models.CharField(max_length=10, blank=False, primary_key=True)
+    photo = models.ImageField(upload_to='photo/%Y/%m/%d/')
+    price = models.CharField(max_length=6, blank=False)
+    stock = models.BooleanField()
+    date = models.DateTimeField()
+    titleManga = models.CharField(max_length=30, blank=False)
     def __str__(self):
         return self.title
 
+
 class manva(models.Model):
- 
-    title = models.CharField(max_length=30,blank=False)
-    year = models.CharField(max_length=4,blank=False)
+    title = models.CharField(max_length=30, blank=False)
+    year = models.CharField(max_length=4, blank=False)
     description = models.TextField()
-    author = models.CharField(max_length=20,blank=False)
-    genre =models.CharField(max_length=10,blank=False)
+    author = models.CharField(max_length=20, blank=False)
+    genre = models.CharField(max_length=10, blank=False)
+    photos = models.ImageField(upload_to='photoManva/%Y/%m/%d/')
+    price = models.CharField(max_length=6, blank=False)
+    stock = models.BooleanField()
+    date = models.DateTimeField()
 
 class ranobe(models.Model):
-  
-    title = models.CharField(max_length=30,blank=False)
-    year = models.CharField(max_length=4,blank=False)
+    title = models.CharField(max_length=30, blank=False)
+    year = models.CharField(max_length=4, blank=False)
     description = models.TextField()
-    author = models.CharField(max_length=20,blank=False)
-    genre =models.CharField(max_length=10,blank=False)
+    author = models.CharField(max_length=20, blank=False)
+    genre = models.CharField(max_length=10, blank=False)
+    photo = models.ImageField(upload_to='photo/%Y/%m/%d/')
+    price = models.CharField(max_length=6, blank=False)
+    stock = models.BooleanField()
+    date = models.DateTimeField()
