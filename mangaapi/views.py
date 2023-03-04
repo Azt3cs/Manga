84439f4f6c5c18213c0c33dc65ff435637c64b18
Alexandr Krainukov <krainukovaleksandr1@gmail.com>
2023-03-04 19:18:09 +0300

    авторизация

--- mangaapi/views.py
+++ mangaapi/views.py
@@ -3,13 +3,17 @@ from rest_framework import generics, mixins
 from django.http import HttpResponse
 from .serializers import *
 from .models import *
-
+from django.contrib.auth.forms import AuthenticationForm
 from rest_framework.viewsets import GenericViewSet
-
+from django.contrib.auth.views import LoginView
 
 def index_page(request):
     manga = Manga.objects.all()
     return render(request, 'mangaapi/index.html',{"manga":manga})
+
+class LoginUser(DataMixin,LoginView):
+    form_class = AuthenticationForm
+    template_name
 
 
 # class MangaViewSet(mixins.CreateModelMixin,
